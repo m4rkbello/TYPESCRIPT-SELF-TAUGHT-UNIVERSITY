2e95f93a78cf9418701294186cb439dfb05c8d0b
m4rkbello <markbello10041999@gmail.com>
2025-11-01 04:45:08 +0800

    ADDED

---
+++ tutorial.ts
@@ -0,0 +1,24 @@
+//para sa number
+let id: number = 5;
+
+//para sa strings
+let programming: string = "alaw utsbo";
+
+//para sa boolean
+let isPatotoya: boolean = false;
+
+//para sa  array
+let numberRange: number[] = [5,10,15,20,25];
+
+//para sa array, string, boolean, number
+let assorted: any = "qwerty101";
+
+//para sa array, string, boolean, number
+let assortedData:  any[] = [1212,"3434343fdgdfg",true,];
+
+//ADD CONCATENATION MEANS SI PALDO UG PATOTOYA ISUMPAY PALDO PATOTOYA
+ayaw =  "bata pako kol";
+kol =  "pandesal init";
+const concatValue = (ayaw, kol) =>{
+    return ayaw + kol;
+};
