ef582df373014e885abd5bef90c1f00402a120d2
m4rkbello <markbello10041999@gmail.com>
2025-11-01 04:50:05 +0800

    ADDED

---
+++ tutorial.js
@@ -0,0 +1,18 @@
+//para sa number
+// let id: number = 5;
+//para sa strings
+var programming = "alaw utsbo";
+//para sa boolean
+var isPatotoya = false;
+//para sa  array
+var numberRange = [5, 10, 15, 20, 25];
+//para sa array, string, boolean, number
+var assorted = "qwerty101";
+//para sa array, string, boolean, number
+var assortedData = [1212, "3434343fdgdfg", true,];
+//ADD CONCATENATION MEANS SI PALDO UG PATOTOYA ISUMPAY PALDO PATOTOYA
+// ayaw =  "bata pako kol";
+// kol =  "pandesal init";
+// const concatValue = (ayaw, kol) =>{
+//     return ayaw + kol;
+// };
